2e47d950aca0bb19baa5cde07bb671e04399fc19
Sanan4li <Alisanan620@gmail.com>
2019-11-29 14:38:23 +0500

    add to cart is working with number increase in badge icon

---
+++ Store/Reducers/CartReducers.js
@@ -0,0 +1,24 @@
+const initialState = {
+    cartItems : [],
+    itemsCount : 0
+
+}
+  
+export default (state=initialState, action)=>{
+    if(action.type=="ADD_TO_CART"){
+        let updatedCartItems = [...state.cartItems, action.item];
+        let count = state.itemsCount + 1;
+         console.log(updatedCartItems);
+         console.log(count);
+      // console.log(state.itemsCount);
+       return {
+        ...state,
+        itemsCount : count,
+        cartItems : updatedCartItems ,
+       
+       } 
+    }
+    return state
+}
+
+
