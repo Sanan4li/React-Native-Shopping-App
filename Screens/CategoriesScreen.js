cc44cf05f92ce3d632b37d2f2656bf98c924d9c1
Sanan4li <Alisanan620@gmail.com>
2019-12-03 02:48:48 +0500

    Data Edited, Hopefully It will work well. Just need to check on actual device

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -14,11 +14,11 @@ class CategoriesScreen extends Component {
   state = {
     categories : [
       {id : "1" , title: "Books", image : "https://image.flaticon.com/icons/png/512/562/562132.png"},
-      {id : "2" , title: "Bags", image : "https://cdn0.iconfinder.com/data/icons/tutor-icon-set/512/Backpack_icon-512.png"},
+      {id : "2" , title: "Mobiles", image : "https://icon-library.net/images/smartphone-icon-png/smartphone-icon-png-13.jpg"},
       {id : "4" , title: "Watches", image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcHVsPJmUpbX1qe0js7-NLLHWzrJNofP0wrim8dle-Oj5k31CC&s"},
       {id : "5" , title: "Shoes", image : "https://cdn3.iconfinder.com/data/icons/sport-set-1/512/Shoes_2-256.png"},
       {id : "8" , title: "Laptops", image : "https://cdn0.iconfinder.com/data/icons/devices-icons-rounded/110/Laptop-512.png"},
-      {id : "9" , title: "T-Shirts", image : "https://cdn2.iconfinder.com/data/icons/mobile-device/512/tshirt-wear-sport-man-blue-round-512.png"},
+      {id : "9" , title: "T-Shirt", image : "https://cdn2.iconfinder.com/data/icons/mobile-device/512/tshirt-wear-sport-man-blue-round-512.png"},
       {id : "6" , title: "Jewelry", image : "https://www.shareicon.net/data/512x512/2016/09/02/824427_jewel_512x512.png"},
       {id : "7" , title: "Wallets", image : "https://cdn4.iconfinder.com/data/icons/peppyicons-rounded/512/wallet2-512.png"},
      
